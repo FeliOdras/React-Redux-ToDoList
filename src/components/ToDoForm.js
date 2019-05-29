d006730c66c8027c04852efbb4be4d9f86f1c789
FeliOdras <ariane.feldmann@odras.de>
2019-05-29 14:40:31 +0200

    Added event listener to form

--- src/components/ToDoForm.js
+++ src/components/ToDoForm.js
@@ -8,7 +8,7 @@ class ToDoForm extends React.Component {
 
     render() {
         return (
-            <form className="input-group my-3">
+            <form className="input-group my-3" onSubmit={this.handleSubmit}>
                 <input
                     className="form-control"
                     name="name"
